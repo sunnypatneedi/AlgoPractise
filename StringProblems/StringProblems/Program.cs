2b0859f2eea8db0c9d92094cc4a4fb41460acb80
Sunny Patneedi <sunnyp@outlook.com>
2015-05-23 09:31:18 -0700

    ReverseString code

---
+++ StringProblems/StringProblems/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            while (Console.ReadLine()!="q")
+            {
+                Console.WriteLine("Enter INPUT: ");
+                string input = Console.ReadLine();
+                Console.WriteLine("OUTPUT >>>>>>: " + ReverseString.ReverseStringInPlace(input.ToCharArray()));
+                Console.WriteLine("(Type 'q' to quit OR hit Enter to continue.)");
+            }   
+        }
+    }
+}
---
+++ StringProblems/StringProblems/ReverseString.cs
@@ -0,0 +1,46 @@
+﻿//JUST A SHOWCASE SOLUTION FOR PRACTISE
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringProblems
+{
+    class ReverseString
+    {
+        //Input - Hello, sunny, yes
+        //Output - 0lleH, ynnus, sey
+
+        //In-place reversal
+        //doesn't consume auxiliary memory but limits usages of the method
+        public static string ReverseStringInPlace(char[] chars)
+        {
+            if (chars.Length == 0) return String.Empty;
+            int j = chars.Length - 1;
+            for (int i = 0; i < chars.Length / 2; i++)
+            {
+                char c = chars[i];
+                chars[i] = chars[j];
+                chars[j] = c;
+                j--;
+            }
+            return new string(chars);
+        }
+        //New String 
+        public static string ReverseStringWithNewString(string str)
+        {
+            if (string.IsNullOrEmpty(str)) return String.Empty;
+            char[] chars = str.ToCharArray();
+            int j = str.Length - 1;
+            for (int i = 0; i < str.Length / 2; i++)
+            {
+                char c = chars[i];
+                chars[i] = chars[j];
+                chars[j] = c;
+                j--;
+            }
+            return new string(chars);
+        }
+    }
+}
